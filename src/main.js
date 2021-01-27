646b05a2abd6aedd5c27d70ce1f51ac00e5ee517
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-27 18:46:35 +0200

    add all divs
    with the values that they needed

--- src/main.js
+++ src/main.js
@@ -4,22 +4,30 @@ const addButton = document.getElementById("add-button");
 const input = document.getElementById("text-input");
 const viewSection = document.getElementById("view");
 const selectedNum = document.getElementById("priority-selector");
-let div = document.createElement("div");
-viewSection.append(div);
 
-addButton.addEventListener("click", () => {
-     let container = document.createElement("div");
+addButton.addEventListener("click", (event) => {
+     const container = document.createElement("div");
      container.classList.add("todo-container");
 
-     let toDoItem = document.createElement("div");
+     const toDoItem = document.createElement("div");
      toDoItem.classList.add("todo-text");
      toDoItem.textContent = input.value;
 
-     let taskPriorrity = document.createElement("div");
+     const taskPriorrity = document.createElement("div");
      taskPriorrity.classList.add("todo-priority");
+     taskPriorrity.textContent = selectedNum.value;
 
+     const createdAt = document.createElement("div");
+     createdAt.classList.add("todo-created-at");
+     createdAt.textContent = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+     container.append(toDoItem);
+     container.append(taskPriorrity);
+     container.append(createdAt);
+     viewSection.append(container);
 
      input.value = ""
 
      });
 });
+
