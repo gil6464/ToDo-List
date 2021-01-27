c8e63be95f41e96e9a5155036d4869cbac70d732
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-27 22:41:40 +0200

    start with the local storage
    by the if statment and the todoarray

--- src/main.js
+++ src/main.js
@@ -5,28 +5,36 @@ const input = document.getElementById("text-input");
 const viewSection = document.getElementById("view");
 const selectedNum = document.getElementById("priority-selector");
 
-let counter = document.getElementById("counter");
-let numberOfTask = 0;
-counter.append(count);
-let count = numberOfTask + " TODOs"
+// let counter = document.getElementById("counter");
+// let numberOfTask = 0;
+// let count = numberOfTask + " TODOs"
+// counter.append(count);
+let todoArray;
+
+if (localStorage.getItem("todoArray") === null) {
+     todoArray= [];
+ } else {
+      todoArray.push(JSON.parse(localStorage))
+ }
+
 
 addButton.addEventListener("click", () => {
-     const container = document.createElement("div");
-     container.classList.add("todo-container");
-     
-     const toDoItem = document.createElement("div");
-     toDoItem.classList.add("todo-text");
-     toDoItem.textContent = input.value;
-     
-     const taskPriorrity = document.createElement("div");
-     taskPriorrity.classList.add("todo-priority");
-     taskPriorrity.textContent = selectedNum.value;
-     
-     const createdAt = document.createElement("div");
-     createdAt.classList.add("todo-created-at");
-     createdAt.textContent = new Date().toISOString().slice(0, 19).replace('T', ' ');
-     
-     
+
+const container = document.createElement("div");
+container.classList.add("todo-container");
+
+const toDoItem = document.createElement("div");
+toDoItem.classList.add("todo-text");
+toDoItem.textContent = input.value;
+
+const taskPriorrity = document.createElement("div");
+taskPriorrity.classList.add("todo-priority");
+taskPriorrity.textContent = selectedNum.value;
+
+const createdAt = document.createElement("div");
+createdAt.classList.add("todo-created-at");
+createdAt.textContent = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
      container.append(toDoItem);
      container.append(taskPriorrity);
      container.append(createdAt);
@@ -35,7 +43,12 @@ addButton.addEventListener("click", () => {
      input.value = ""
      numberOfTask ++;
      
-     console.log(numberOfTask);
+     // const sortButton = document.getElementById("sort-button");
+     // sortButton.addEventListener("click", () =>{
+          // const task = document.querySelectorAll(".todo-priority") 
+          // console.log(task.textContent)
+          // })
+          
      });
+     
 });
-
