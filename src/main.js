a168e21a0bfe88e29d29c9dc334bcf7320b2ddfd
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-27 18:16:48 +0200

    start making the divs
    when clicking the add button, it will crate the divs with the needed class

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
 
 const addButton = document.getElementById("add-button");
 const input = document.getElementById("text-input");
 const viewSection = document.getElementById("view");
+const selectedNum = document.getElementById("priority-selector");
 let div = document.createElement("div");
+viewSection.append(div);
 
 addButton.addEventListener("click", () => {
-     div.append(input.value);
-     viewSection.append(div);
-     input.value = ""
-          
-     });
+     let container = document.createElement("div");
+     container.classList.add("todo-container");
 
-})
+     let toDoItem = document.createElement("div");
+     toDoItem.classList.add("todo-text");
+     toDoItem.textContent = input.value;
+
+     let taskPriorrity = document.createElement("div");
+     taskPriorrity.classList.add("todo-priority");
+
+
+     input.value = ""
+
+     });
+});
