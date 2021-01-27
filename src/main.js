b0bd99d68b0c9865672c3ff729abb922e772302b
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-27 17:22:52 +0200

    input progress
    put the input inside the div in the view section. and made him become clear after clicking the add button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-  
+document.addEventListener("DOMContentLoaded", ()=> {
+
+const addButton = document.getElementById("add-button");
+const input = document.getElementById("text-input");
+const viewSection = document.getElementById("view");
+let div = document.createElement("div");
+
+addButton.addEventListener("click", () => {
+     div.append(input.value);
+     viewSection.append(div);
+     input.value = ""
+          
+     });
+
+})
