196a7cb5116bec8af9bded3e7e992d8ed89cff47
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-28 15:50:57 +0200

    solved local storge

--- src/main.js
+++ src/main.js
@@ -5,62 +5,66 @@ const addButton = document.getElementById("add-button");
 const viewSection = document.getElementById("view");
 const selectedNum = document.getElementById("priority-selector");
 
-// let counter = document.getElementById("counter");
-// let numberOfTask = 0;
-// let count = numberOfTask + " TODOs"
-// counter.append(count);
-
 function createContainer () {
      const container = document.createElement("div");
      container.classList.add("todo-container");
      return container;
 }
-function toDoItem (input) {
+function toDoItem (text) {
      const toDoItem = document.createElement("div");
      toDoItem.classList.add("todo-text");
-     toDoItem.textContent = input.value;
+     toDoItem.textContent = text;
      return toDoItem;
 }
-function getPriorrity(selectedNum) {
+function getPriorrity(num) {
      const taskPriorrity = document.createElement("div");
      taskPriorrity.classList.add("todo-priority");
-     taskPriorrity.textContent = selectedNum.value;
+     taskPriorrity.textContent = num;
      return taskPriorrity;
 }
-function getTime() {
+function getTime(time = new Date().getTime()) {
      const createdAt = document.createElement("div"); 
      createdAt.classList.add("todo-created-at");
-     createdAt.textContent = new Date().toISOString().slice(0, 19).replace('T', ' ');
+     createdAt.textContent = new Date(time).toISOString().slice(0, 19).replace('T', ' ');
      return createdAt;
 }
-let todoArray;
+let todoArray= JSON.parse(localStorage.getItem("my-todo"));
+console.log(todoArray)
+if (todoArray === null) {
+     todoArray = [];
+} 
+ for (const data of todoArray) {
+     const container =  createContainer();
+     container.append(toDoItem(data.text));
+     container.append(getPriorrity(data.priority));
+     container.append(getTime(data.date));
+     viewSection.append(container);
+ }
 addButton.addEventListener("click", () => {
      
+     let task = {
+      priority: selectedNum.value,
+      text : input.value,
+      date : new Date().getTime()
+     };
      const container =  createContainer();
-     container.append(toDoItem(input));
-     container.append(getPriorrity(selectedNum));
-     container.append(getTime());
+     container.append(toDoItem(task.text));
+     container.append(getPriorrity(task.priority));
+     container.append(getTime(task.date));
      viewSection.append(container);
      
-     let task = {
-      priority: getPriorrity(selectedNum).textContent,
-      text : toDoItem(input).textContent,
-      time : getTime().textContent
-     };
      input.value = ""
      
-     if (localStorage.getItem("my-todo") === "null") {
-          todoArray.push(task);
-          localStorage.setItem("my-todo", JSON.stringify(todoArray));
-     } else {
-          localStorage.clear("my-todo");
-          todoArray.push(task);
-          localStorage.setItem("my-todo", JSON.stringify(todoArray));
-     }
+     todoArray.push(task);
+     localStorage.setItem("my-todo", JSON.stringify(todoArray));
      
 });
 });
 
 // todoArray.push(JSON.parse(localStorage.getItem("my-todo")));
 // let getBack = JSON.parse(localStorage.getItem("my-todo"))
-          // console.log(getBack);
+// console.log(getBack);
+// let counter = document.getElementById("counter");
+// let numberOfTask = 0;
+// let count = numberOfTask + " TODOs"
+// counter.append(count);
