0516592031604ea9e8e536d6d844de9285bca59b
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-02-17 22:42:45 +0200

    added post

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
-console.log(express);
 const fs = require('fs');
-const app = express;
+const app = express();
 app.use(express.json())
 
 app.get('/v3/b/:id', (req, res) => {
     const id = req.params.id;
+    console.log(id);
     try {
-        const binContent = fs.readFileSync(`./bins/${id}.json`);
+        const binContent = fs.readFileSync(`./bins/${id}.json`, 
+        {encoding:'utf8', flag:'r'});
+        console.log(binContent);
         res.send(binContent);
     } catch(e) {
-        res.status(422).json({"message": "Invalid Record ID"});
+        res.status(422).json({"message": `Invalid Record: ${id}`});
     } 
 })
 
@@ -30,13 +32,38 @@ app.put('/v3/b/:id', (req, res) => {
     const successMessage = {
         success : true,
         data : body,
-        "verison" : 1,
+        "version" : 1,
         "parentId" : id
     }
     res.send(successMessage);
     
 })
 
+app.post('/v3/b/', (req, res) => {
+    const {body} = req.body;
+    const id = uuid.v4;
+    body.id = id;
+    fs.writeFileSync(`./backend/bins/${id}.json`, JSON.stringify(body, null , 4),
+    (err) => {
+        if (err) {
+            res.send("error");
+        } else {
+            res.send(body)
+        }   
+    });
+})
+
 app.listen(3000, () => {
     console.log("app is running on port 3000")
 });
+
+    
+      (err) => {
+        if (err) {
+          res.send("error");
+        } else {
+          res.send(body);
+        }
+      }
+    );
+  });
