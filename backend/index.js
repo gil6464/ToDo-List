e440bd280944ffff55d346a77a56ec8bc933f683
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-02-17 21:32:01 +0200

    write write and get methods

--- backend/index.js
+++ backend/index.js
@@ -1,112 +1,42 @@
-// const {days} = require('./myModule');
-// const {readFileSync} = require('fs');
-// const http = require('http');
-// console.log(http);
-// const {x} = require('./myModule');
-// const fromModule = require('./myModule');
-
-// console.log(days);
-// console.log(fromModule);
-// console.log(x);
-//  
-
-// console.log(data); 
-
-// const server = http.createServer(function(request, response){
-//     if(request.url === '/task'){
-//         const data = readFileSync('./task.html', 
-//             {encoding:'utf8', flag:'r'});
-//         response.write(data);
-//         response.end();
-//     }
-//     if(request.url === '/notTask'){
-//         const data = readFileSync('./notTask.html', 
-//             {encoding:'utf8', flag:'r'});
-//         response.write(data);
-//         response.end();
-//     }
-//     if(request.method === 'POST'){
-//         response.write('post method');
-//         response.end();
-//     }
-//     response.end();
-// });
-// server.listen(3001);
-
-// GET myapi.com:3001/tasks - יחזיר את כל הTASKS
-// GET myapi.com:3001/task/123 - יחזיר את המשימה שהאיידי הוא 123
-// POST myapi.com:3001/task/ - שולחים משימה חדשה
-// PUT myapi.com:3001/task/ - מעדכנים
-// DELETE myapi.com:3001/task/ - מוחקים משימה
-
-
 const express = require('express');
 console.log(express);
-// const app = express();
-// app.use(express.json()); // כשמקבלים בבאדי ג'ייסון אז הוא הופך לאובייקט
+const fs = require('fs');
+const app = express;
+app.use(express.json())
 
-// const {readFileSync} = require('fs');
-// let tasks = [
-//     {
-//         id: '4fw34y',
-//         title: 'doing node-express task',
-//         dueDate: '18.02.2021',
-//         category: 'programming stuying',
-//         isDone: false,
-//     },
-//     {
-//         id: 'er5776e',
-//         title: 'watching node lecture',
-//         dueDate: '18.02.2021',
-//         category: 'programming stuying',
-//         isDone: false,
-//     },
-//     {
-//         id: 'a46s5e6s5u',
-//         title: 'watching express lecture',
-//         dueDate: '18.02.2021',
-//         category: 'programming stuying',
-//         isDone: false,
-//     },
-// ];
+app.get('/v3/b/:id', (req, res) => {
+    const id = req.params.id;
+    try {
+        const binContent = fs.readFileSync(`./bins/${id}.json`);
+        res.send(binContent);
+    } catch(e) {
+        res.status(422).json({"message": "Invalid Record ID"});
+    } 
+})
 
-// app.get('/tasks', (req, res)=>{
-//     res.send(tasks)
-// });
+app.put('/v3/b/:id', (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    const binExist = fs.existsSync('./bins/${id}.json');
 
-// app.get('/task/:id', (req, res)=>{
-//     const id = req.params.id;
-//     const data = readFileSync('./'+id+'.JSON', 
-//             {encoding:'utf8', flag:'r'});
-//     res.send(data);
-// for(let task of tasks){
-//     if(task.id === id){
-//         res.send(task);
-//     }
-// }
-// });
+    if(!binExist) {
+        res.status(404).json({
+            "message" : "Bin not found",
+            "success" : false
+        });
+        return;
+    }
+    fs.writeFileSync(`./bins/${id}.json`, JSON.stringify(body, null , 4));
+    const successMessage = {
+        success : true,
+        data : body,
+        "verison" : 1,
+        "parentId" : id
+    }
+    res.send(successMessage);
+    
+})
 
-// app.post('/task',(req, res)=>{
-//     tasks.push(req.body);
-//     res.send('ok');
-// });
-
-// app.put('/task',(req, res)=>{
-//     for(let i = 0; i< tasks.length; i++){
-//         if(tasks[i].id === req.body.id){
-//            tasks[i] = req.body;
-//         res.send(req.body);
-//         }
-//     }
-// });
-
-// app.delete('/task',(req, res)=>{
-//     for(let i = 0; i< tasks.length; i++){
-//         if(tasks[i].id === req.body.id){
-//            tasks.splice(i, 1);
-//             res.send('removed');
-//         }
-//     }
-// });
-
-// app.listen(3000);
+app.listen(3000, () => {
+    console.log("app is running on port 3000")
+});
